63f9841d7a3982256675d56aa96421b24272baa6
Salil Singh <salil.singh@ad.infosys.com>
2016-06-08 13:21:47 +0530

    sixth commit

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -14,7 +14,7 @@ namespace WebApplication4
         {
             if(!IsPostBack)
             {
-                str = "inside postback newone again new fifth";
+                str = "inside postback newone again new fifth sixth";
             }
         }
     }
