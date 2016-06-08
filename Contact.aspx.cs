f35f6d525659a9f603c9992a7d32f83c15eb5cd1
Salil Singh <salil.singh@ad.infosys.com>
2016-06-08 12:04:03 +0530

    thr build

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -14,7 +14,7 @@ namespace WebApplication4
         {
             if(!IsPostBack)
             {
-                str = "inside postback newone";
+                str = "inside postback newone again";
             }
         }
     }
