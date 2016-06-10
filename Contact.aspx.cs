ecf1764527af6b03bb4d8a795ab84e585b6d62df
Salil Singh <salil.singh@ad.infosys.com>
2016-06-10 17:12:06 +0530

    friday commit

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -14,7 +14,7 @@ namespace WebApplication4
         {
             if(!IsPostBack)
             {
-                str = "inside postback newone again new fifth sixth seventh";
+                str = "inside postback newone again new fifth sixth seventh eighth";
             }
         }
     }
